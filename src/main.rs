9870c328ba6dd87dc3b6176053a0d3f275068ba9
kjain1810 <kunaljain.2000@yahoo.in>
2021-06-17 01:07:08 +0530

    VGA buffer done

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,19 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {
+mod vga_buffer;
 
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
+
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("os lol");
     loop {}
 }
